0739230a819f6f4e786fb384781decd43c85e0e0
Syed Haris <syedmdharis@hotmail.com>
2021-03-07 14:26:10 -0600

    New solution

---
+++ Datacap915.Library.NuGet/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datacap915.Library.NuGet
+{
+    public class Class1
+    {
+    }
+}
